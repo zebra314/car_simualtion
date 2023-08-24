c08e0b2492da93b6f431161996db2c468ac76a7c
zebra314 <user.name>
2023-08-24 14:16:39 +0800

    Enable get ip automatically

--- pollo_teleop/nodes/generate_qrcode.py
+++ pollo_teleop/nodes/generate_qrcode.py
@@ -1,3 +1,24 @@
 import qrcode_terminal
-url = "http://192.168.0.153:8888"
+import socket
+import time
+import os
+
+# Get local ip
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip = s.getsockname()[0]
+s.close()
+
+# Write local ip to local_ip.js
+current_directory = os.path.dirname(os.path.realpath(__file__))
+relative_path = "../rosboard/html/include/local_ip.js"
+file_path = os.path.join(current_directory, relative_path)
+
+with open (file_path, "w") as f:
+  print('Writing local ip to local_ip.js')
+  f.write(f'export let local_ip = "{ip}";')
+
+# Generate QR code in terminal
+url = "http://" + ip + ":8888"
+time.sleep(1)
 qrcode_terminal.draw(url)
--- pollo_teleop/rosboard/html/include/joystickControl.js
+++ pollo_teleop/rosboard/html/include/joystickControl.js
@@ -1,39 +1,15 @@
-var ros = new ROSLIB.Ros({
-  // Change to robot ip address 
-  // 9090 is the default port of rosbridge server
-  url: 'ws://192.168.0.153:9090'
-});
+import ros from './rosConnection.js';
 
-ros.on('connection', function () {
-  document.getElementById("status").innerHTML = "Connected";
-});
-
-ros.on('error', function (error) {
-  document.getElementById("status").innerHTML = "Error";
-});
-
-ros.on('close', function () {
-  document.getElementById("status").innerHTML = "Closed";
-});
-
-var txt_listener = new ROSLIB.Topic({
-  ros: ros,
-  name: '/txt_msg',
-  messageType: 'std_msgs/String'
-});
-
-txt_listener.subscribe(function (m) {
-  document.getElementById("msg").innerHTML = m.data;
-  move(1, 0);
-});
-
-cmd_vel_listener = new ROSLIB.Topic({
+var cmd_vel_listener = new ROSLIB.Topic({
   ros: ros,
   name: "/pollo/cmd_vel",
   messageType: 'geometry_msgs/Twist'
 });
 
-move = function (linear, angular) {
+var linear_speed = 0;
+var angular_speed = 0;
+
+function move(linear, angular) {
   var twist = new ROSLIB.Message({
     linear: {
       x: linear,
@@ -49,7 +25,7 @@ move = function (linear, angular) {
   cmd_vel_listener.publish(twist);
 }
 
-createJoystick = function () {
+function createJoystick() {
   var options = {
     zone: document.getElementById('zone_joystick'),
     threshold: 0.1,
@@ -58,11 +34,9 @@ createJoystick = function () {
     size: 150,
     color: '#000000',
   };
-  manager = nipplejs.create(options);
+  var manager = nipplejs.create(options);
 
-  linear_speed = 0;
-  angular_speed = 0;
-
+  var timer = null;
   manager.on('start', function (event, nipple) {
     timer = setInterval(function () {
       move(linear_speed, angular_speed);
@@ -70,9 +44,9 @@ createJoystick = function () {
   });
 
   manager.on('move', function (event, nipple) {
-    max_linear = 1.7; // m/s
-    max_angular = 1.3; // rad/s
-    max_distance = 75.0; // pixels;
+    var max_linear = 1.7; // m/s
+    var max_angular = 1.3; // rad/s
+    var max_distance = 75.0; // pixels;
     linear_speed = Math.sin(nipple.angle.radian) * max_linear * nipple.distance / max_distance;
     angular_speed = -Math.cos(nipple.angle.radian) * max_angular * nipple.distance / max_distance;
   });
@@ -81,7 +55,7 @@ createJoystick = function () {
     if (timer) {
       clearInterval(timer);
     }
-    self.move(0, 0);
+    move(0, 0);
   });
 }
 
